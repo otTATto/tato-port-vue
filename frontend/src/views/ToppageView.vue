8225e6f70ef50d3b60b2e3b5e1561eba37d644c2
otTATto <TATo123SU@gmail.com>
2025-04-12 02:15:12 +0900

    fix: WebKit ベースかどうかで描画処理を分岐 (#4)

--- frontend/src/views/ToppageView.vue
+++ frontend/src/views/ToppageView.vue
@@ -1,65 +1,143 @@
 <template>
   <div class="flex items-center justify-center h-full">
-    
-    <!-- 中央のモットー -->
-    <img src="../img/motto/motto.webp" 
-      alt="Motto" 
-      :class="['w-[88vw] md:w-[70vw] xl:w-[50vw]', isShrunk ? 'shrink-animation' : '']"  
-    />
 
-    <!-- 上部ギャラリー -->
-    <div class="scroll-infinity absolute top-10 w-full overflow-hidden gallery-fade-in">
-      <div class="scroll-infinity__wrap gallery-row">
-        <ul class="scroll-infinity__list scroll-infinity__list--right">
-          <li v-for="(img, i) in topImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
-            <img :src="img" alt="Gallery Image"
-              class="rounded-lg"
-            />
-          </li>
-        </ul>
-        <ul class="scroll-infinity__list scroll-infinity__list--right">
-          <li v-for="(img, i) in topImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
-            <img :src="img" alt="Gallery Image"
-              class="rounded-lg"
-            />
-          </li>
-        </ul>
-        <ul class="scroll-infinity__list scroll-infinity__list--right">
-          <li v-for="(img, i) in topImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
-            <img :src="img" alt="Gallery Image"
-              class="rounded-lg"
-            />
-          </li>
-        </ul>
-      </div>
-    </div>
+    <!-- iOS 向けのギャラリー -->
 
-    <!-- 下部ギャラリー -->
-    <div class="scroll-infinity absolute bottom-10 w-full overflow-hidden gallery-fade-in">
-      <div class="scroll-infinity__wrap gallery-row">
-        <ul class="scroll-infinity__list scroll-infinity__list--left">
-          <li v-for="(img, i) in bottomImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
-            <img :src="img" alt="Gallery Image"
-              class="rounded-lg"
-            />
-          </li>
-        </ul>
-        <ul class="scroll-infinity__list scroll-infinity__list--left">
-          <li v-for="(img, i) in bottomImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
-            <img :src="img" alt="Gallery Image"
-              class="rounded-lg"
-            />
-          </li>
-        </ul>
-        <ul class="scroll-infinity__list scroll-infinity__list--left">
-          <li v-for="(img, i) in bottomImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
-            <img :src="img" alt="Gallery Image"
-              class="rounded-lg"
-            />
-          </li>
-        </ul>
+      <!-- 中央のモットー -->
+      <img src="../img/motto/motto.webp" 
+        v-if="isWebKit"
+        alt="Motto" 
+        :class="['w-[88vw] md:w-[70vw] xl:w-[50vw]', isShrunk ? 'shrink-animation' : '']"  
+      />
+
+      <!-- 上部ギャラリー -->
+      <div 
+        v-if="isWebKit"
+        class="scroll-infinity absolute top-[5vh] w-full overflow-hidden gallery-fade-in">
+        <div class="mask-x-from-75% mask-x-to-95% scroll-infinity__wrap gallery-row">
+          <ul class="scroll-infinity__list scroll-infinity__list--right">
+            <li v-for="(img, i) in topImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+          <ul class="scroll-infinity__list scroll-infinity__list--right">
+            <li v-for="(img, i) in topImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+          <ul class="scroll-infinity__list scroll-infinity__list--right">
+            <li v-for="(img, i) in topImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+
+      <!-- 下部ギャラリー -->
+      <div 
+        v-if="isWebKit"
+        class="scroll-infinity absolute top-[55vh] w-full overflow-hidden gallery-fade-in">
+        <div class="mask-x-from-75% mask-x-to-95% scroll-infinity__wrap gallery-row">
+          <ul class="scroll-infinity__list scroll-infinity__list--left">
+            <li v-for="(img, i) in bottomImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+          <ul class="scroll-infinity__list scroll-infinity__list--left">
+            <li v-for="(img, i) in bottomImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+          <ul class="scroll-infinity__list scroll-infinity__list--left">
+            <li v-for="(img, i) in bottomImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    <!-- ここまで iOS 向けのギャラリー -->
+
+    <!-- iOS 以外の通常のギャラリー -->
+
+      <!-- 中央のモットー -->
+      <img src="../img/motto/motto.webp" 
+        v-if="!isWebKit"
+        alt="Motto" 
+        :class="['w-[88vw] md:w-[70vw] xl:w-[50vw]', isShrunk ? 'shrink-animation' : '']"  
+      />
+
+      <!-- 上部ギャラリー -->
+      <div 
+        v-if="!isWebKit"
+        class="scroll-infinity absolute top-10 w-full overflow-hidden gallery-fade-in">
+        <div class="scroll-infinity__wrap gallery-row">
+          <ul class="scroll-infinity__list scroll-infinity__list--right">
+            <li v-for="(img, i) in topImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+          <ul class="scroll-infinity__list scroll-infinity__list--right">
+            <li v-for="(img, i) in topImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+          <ul class="scroll-infinity__list scroll-infinity__list--right">
+            <li v-for="(img, i) in topImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- 下部ギャラリー -->
+      <div
+        v-if="!isWebKit" 
+        class="scroll-infinity absolute bottom-10 w-full overflow-hidden gallery-fade-in">
+        <div class="scroll-infinity__wrap gallery-row">
+          <ul class="scroll-infinity__list scroll-infinity__list--left">
+            <li v-for="(img, i) in bottomImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+          <ul class="scroll-infinity__list scroll-infinity__list--left">
+            <li v-for="(img, i) in bottomImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+          <ul class="scroll-infinity__list scroll-infinity__list--left">
+            <li v-for="(img, i) in bottomImages" :key="`top-${i}`" class="scroll-infinity__item mx-2">
+              <img :src="img" alt="Gallery Image"
+                class="rounded-lg"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    <!-- ここまで iOS 以外の通常のギャラリー -->
 
   </div>
 </template>
@@ -67,7 +145,7 @@
 <script setup lang="ts">
   import { ref, onMounted } from 'vue'
 
-  // ギャラリー画像のインポート
+  // @brief ギャラリー画像のインポート
   import topImage1 from '@/img/gallery/top/image1.webp'
   import topImage2 from '@/img/gallery/top/image2.webp'
   import topImage3 from '@/img/gallery/top/image3.webp'
@@ -82,13 +160,44 @@
 
   const bottomImages = [bottomImage1, bottomImage2, bottomImage3, bottomImage4]
 
-  // モットーの縮小状態
-  const isShrunk = ref(false)
+  // === ここから: WebKit ベース向けの処理 ===
 
-  // ギャラリー表示の状態管理
-  const showGallery = ref(false)
+  // @brief WebKit ベースかどうかの判定フラグ
+  const isWebKit = ref<boolean>(false)
+
+  // === ここまで: WebKit ベース向けの処理 ===
+
+  // @brief モットーの縮小状態
+  const isShrunk = ref<boolean>(false)
+
+  // @brief ギャラリー表示の状態管理
+  const showGallery = ref<boolean>(false)
 
   onMounted(() => {
+    // === ここから: WebKit ベース向けの処理 ===
+
+    // @brief iOS, iPad OS, iPod OS かどうか、および Safari ブラウザかどうかの判定
+    const ua = navigator.userAgent
+    // @brief iOS, iPad OS, iPod OS かどうかの判定フラグ
+    // const isIOS = /iPhone|iPad|iPod/i.test(ua)
+    const isIOS: boolean = true
+    // @brief Safari かどうかを判定
+    const isSafari: boolean = /Safari/.test(ua) 
+                           && !/Chrome/.test(ua) 
+                           && !/CriOS/.test(ua)
+                           && !/FxiOS/.test(ua)
+    // @brief WebKit ベースかどうかを判定
+    isWebKit.value = isIOS || isSafari
+    // @brief デバッグ用出力
+    console.log('===== WebKit base or not =====')
+    console.log('User Agent:', ua)
+    console.log('isIOS:', isIOS)
+    console.log('isSafari:', isSafari)
+    console.log('isWebKit:', isWebKit.value)
+    console.log('==============================')
+
+    // === ここまで: WebKit ベース向けの処理 ===
+
     // 2秒後にモットーを縮小する
     setTimeout(() => {
       isShrunk.value = true
@@ -114,7 +223,6 @@
   }
   img {
     display: block;
-    vertical-align: middle;
   }
 
   /* ギャラリー専用のフェードインアニメーション */
