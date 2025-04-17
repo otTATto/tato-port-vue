9cd9d2fe48d535859dd50626b6819c5951b8c67a
otTATto <TATo123SU@gmail.com>
2025-04-18 02:18:11 +0900

    feat(404): 404 ページの実装とルーティング (#6)

---
+++ frontend/src/views/NotfoundView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="bg-TatoGreen h-[80dvh] justify-center flex items-center">
+      <h1 class="text-center">
+        <div class="text-7xl font-black text-white">404</div>
+        <div class="text-2xl text-white font-MaruGothic font-normal"> Not Found</div>
+      </h1>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+    defineOptions({
+      name: 'NotFound',
+    })
+  </script>
+  
+  <style>
+  
+  </style>
