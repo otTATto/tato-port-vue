cfb7b1d49566b95448c914c756ef779f71bdcf48
たと <TATo123SU@gmail.com>
2025-07-08 03:47:00 +0900

    feat: Google アナリティクスの導入 (#31)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './firebase'
 
 // import: Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
